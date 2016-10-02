a7a6f165370d0052101748bdad1cea5f346d68b9
Tylor Garrett <tylorgarrett@gmail.com>
2016-10-02 19:08:20 -0400

    Added jquery

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$("h1").click(function(e) {
-		console.log("You clicked an h1")
-		console.log(e.target.innerText)
-	})
-})
+		console.log("You clicked an h1");
+	});
+});
